6077023285de71a0bab3bbe266c2f1744020a92d
Kam Kennedy <kameronk013@gmail.com>
2024-09-23 11:33:04 -0600

    testing for larger arrays

--- array_largest_element_spec.rb
+++ array_largest_element_spec.rb
@@ -15,4 +15,10 @@ RSpec.describe "largest_number" do
     arr = [100, -100]
     expect(largest_number(arr)).to eq(100)
   end 
+
+  it "can handle larger arrays" do
+    arr = [34, -2, 16, 99, 3, 27, 56, -45, 0, 23, 12, 5, 77, 65, 88, -34, 
+    18, 31, 25, 62, 42, 84, 7, 13, -9, 59, -17, 90, 46, 29]
+    expect(largest_number(arr)).to eq(99)
+  end
 end
